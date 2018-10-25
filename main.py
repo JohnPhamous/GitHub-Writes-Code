cd03d082e5ce0ea9b93c51728308d4192fed6641
Anupam Dagar <dagaranupam@gmail.com>
2018-10-25 23:34:09 +0530

    change to one line

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ class PurpleBananaMonkey(self, monkey_type = "Chimpanzee", color = "Purple"):
 
 def main():
     print("Let's get started!")
-    print("Please choose from the following")
-    print("1. GitHub\n2. Facebook\n3. Twitter") # Add more choices here
-    choice = int(input())
+    choice = int(input("Please choose from the following\n1. GitHub\n2. Facebook\n3. Twitter\n")) # Add more choices here
 
 if __name__ == "__main__":
     main()
