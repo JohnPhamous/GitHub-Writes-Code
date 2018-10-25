31f4c8a9a4ce7666537f30464a640fc4f66d5b26
Anupam Dagar <dagaranupam@gmail.com>
2018-10-25 23:30:22 +0530

    add choices and input for choices on program startup

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ class PurpleBananaMonkey(self, monkey_type = "Chimpanzee", color = "Purple"):
 
 def main():
     print("Let's get started!")
+    print("Please choose from the following")
+    print("1. GitHub\n2. Facebook\n3. Twitter") # Add more choices here
+    choice = int(input())
 
 if __name__ == "__main__":
     main()
