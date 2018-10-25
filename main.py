64e2d26809806a4fe8bd95cc61109f0edab3ddd5
Scott <SCOTTLB@users.noreply.github.com>
2018-10-25 10:36:01 +0100

    Christmas related code
    It's almost Christmas time! Lets draw a snowflake :santa:

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+def draw_snowflake():
+    from turtle import Turtle
+    t = Turtle()
+    t.speed(0)
+    #a = 180
+    b = 180
+    for c in range(5):
+        a = 9*c
+        for i in range(100):
+            t.circle(i,a)
+            t.right(b)
+            t.circle(i,a)
+            t.right(b)
+            t.circle(i,a)
+            t.right(b)
+            t.circle(i,a)
+
+
 def main():
     print("Let's get started!")
+    draw_snowflake()
 
 if __name__ == "__main__":
     main()
