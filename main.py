2b2e83248c08716c2aec92262ca0c5217b0559d6
William Shiao <willshiao@gmail.com>
2018-10-26 11:56:43 -0700

    Print a tree (in one line)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ class PurpleBananaMonkey(self, monkey_type = "Chimpanzee", color = "Purple"):
     def __init__():
         
 def main():
+    print('\n'.join(' ' * (10-x) + '*' * (x * 2 + 1) for x in range(10)) + '\n' + '\n'.join(' ' * 8 + '|' * 5 for x in range(4)))
     print("Let's get started!")
     choice = int(input("Please choose from the following\n1. GitHub\n2. Facebook\n3. Twitter\n")) # Add more choices here
 
