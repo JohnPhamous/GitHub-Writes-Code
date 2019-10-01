6318da3b41f491c5af482990c1aa770b9efddaa7
Juan Ramos <juancagb.17@gmail.com>
2019-10-01 02:32:11 -0500

    Fix class syntax and add another choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-#new line here
-class PurpleBananaMonkey(self, monkey_type = "Chimpanzee", color = "Purple"):  
-    def __init__(monkey_type,color):
+class PurpleBananaMonkey:  
+    def __init__(self, monkey_type = "Chimpanzee", color = "Purple"):
         self.monkey_type = monkey_type
         self.color = color
      
@@ -10,13 +9,14 @@ class PurpleBananaMonkey(self, monkey_type = "Chimpanzee", color = "Purple"):
 
 def main():
     print('\n'.join(' ' * (10-x) + '*' * (x * 2 + 1) for x in range(10)) + '\n' + '\n'.join(' ' * 8 + '|' * 5 for x in range(4)))
-    print("Let's get started!")
-
-    
+    print("Let's get started!")   
 
     choice = int(input("Please choose from the following\n1. GitHub\n2. Facebook\n3. Twitter\n")) # Add more choices here
+    
     if choice == 1:
         print("https://github.com")
+    elif choice == 2:
+        print("https://facebook.com")
 
 
 if __name__ == "__main__":
